fcae3da817c64ab4a897b3baa5d3d123869901e8
edwardb0911 <EdwardB911@hotmail.com>
2019-12-04 19:58:13 -0500

    Trying to flip image, got syntax error.

--- main.py
+++ main.py
@@ -15,18 +15,17 @@ new_img = Image.new("RGB", (width, height), "white")
 
 # TODO: Replace this with your own filter!
 # Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [0] * 9
-for i in range(2, width // 2):
-    for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        new_img.putpixel((i, j), (random.choice(members)))
+#members = [0] * 9
+for i in range(0, width):
+    for j in range(0, height):
+        #members[1] = img.getpixel((i - 1, j))
+        #members[2] = img.getpixel((i - 1, j + 1))
+        #members[3] = img.getpixel((i, j - 1))
+        #members[4] = img.getpixel((i, j))
+        #members[5] = img.getpixel((i, j + 1))
+        #members[6] = img.getpixel((i + 1, j - 1))
+        #members[7] = img.getpixel((i + 1, j))
+        #members[8] = img.getpixel((i + 1, j + 1))
+        new_img.putpixel((i, j), (img.getpixel(width - i, height - j)))
 
 new_img.save(output_path)
