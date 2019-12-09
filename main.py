4ca80e2352b5966b0ca729283d990c71a159a414
edwardb0911 <EdwardB911@hotmail.com>
2019-12-08 19:12:52 -0500

    Properly commented

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ width, height = img.size
 # Create a new, all-white image that's the same size as the original
 new_img = Image.new("RGB", (width, height), "white")
 
-# TODO: Replace this with your own filter!
-# Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-#members = [0] * 9
+
 for i in range(1, width - 1):
     for j in range(1, height - 1):
         #flips image upside down
