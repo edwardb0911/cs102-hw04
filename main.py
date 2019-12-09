d8f1aba527ccf94e35667aeb43900c7a438165af
edwardb0911 <EdwardB911@hotmail.com>
2019-12-08 19:11:06 -0500

    Properly commented

--- main.py
+++ main.py
@@ -18,14 +18,7 @@ new_img = Image.new("RGB", (width, height), "white")
 #members = [0] * 9
 for i in range(1, width - 1):
     for j in range(1, height - 1):
-        #members[1] = img.getpixel((i - 1, j))
-        #members[2] = img.getpixel((i - 1, j + 1))
-        #members[3] = img.getpixel((i, j - 1))
-        #members[4] = img.getpixel((i, j))
-        #members[5] = img.getpixel((i, j + 1))
-        #members[6] = img.getpixel((i + 1, j - 1))
-        #members[7] = img.getpixel((i + 1, j))
-        #members[8] = img.getpixel((i + 1, j + 1))
+        #flips image upside down
         x = width - i
         y = height - j
         new_img.putpixel((i, j), (img.getpixel((x, y))))
